cfbd28716e69c720e91bc820fac4eb8344df3bbe
Ben Altman <altmanguitar@gmail.com>
2020-12-19 02:48:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,15 +5,15 @@ class Dog
 
   attr_accessor :name
 
-  def save
-    @@all << self
-  end
-
   def initialize(name)
     @name = name
     self.save
   end
 
+  def save
+    @@all << self
+  end
+  
   def self.all
     @@all
   end
